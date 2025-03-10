d63b1bfc361c2790b78218b50ed063ead5e49f36
hyerin-kang <hkang0420@gmail.com>
2025-03-10 14:45:37 +0900

    fetch로 todo데이터 받아오기

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import { getTodos } from "@/api/todo-api";
 import Link from "next/link";
 
-export default function Home() {
+const HomePage = async () => {
+  const todos = await getTodos();
   return (
     <div>
       HOMEPAGE
-      <Link href="/detail">Detail Page</Link>
+      <br /> <Link href="/detail">Detail Page</Link>
+      <ul>
+        {todos.map(({ id, text, completed }) => (
+          <li key={id}>
+            <p>
+              {text} {completed ? "(completed)" : "(pending)"}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default HomePage;
