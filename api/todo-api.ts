a4fe7fb1f73fa53087b75b9c8922a81a93b4669b
hyerin-kang <hkang0420@gmail.com>
2025-03-11 09:41:40 +0900

    csr컴포넌트

--- api/todo-api.ts
+++ api/todo-api.ts
@@ -1,7 +1,7 @@
 import { Todo } from "@/types/todo.type";
 
 export const getTodos = async () => {
-  const response = await fetch("http://localhost:3000/todos");
+  const response = await fetch("http://localhost:4000/todos");
   const data: Todo[] = await response.json();
   return data;
 };
