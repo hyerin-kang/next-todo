f7b551bb2c980de681e9c12a59c23ffcbbbca55e
hyerin-kang <hkang0420@gmail.com>
2025-03-13 20:42:04 +0900

    feat: todo API 기능 추가

--- api/todo-api.ts
+++ api/todo-api.ts
@@ -1,7 +1,38 @@
 import { Todo } from "@/types/todo.type";
-
+const BASE_URL = "http://localhost:4000/todos";
 export const getTodos = async () => {
-  const response = await fetch("http://localhost:4000/todos");
+  const response = await fetch(BASE_URL);
   const data: Todo[] = await response.json();
+  return data;
+};
+
+export const createTodo = async (text: Todo["text"]) => {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text, completed: false }),
+  });
+  const data: Todo = await response.json();
+  return data;
+};
+
+export const deleteTodo = async (id: Todo["id"]) => {
+  const response = await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
+  const data: Todo = await response.json();
+  return data;
+};
+
+export const toggleTodo = async (
+  id: Todo["id"],
+  completed: Todo["completed"]
+) => {
+  const response = await fetch(`${BASE_URL}/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ completed }),
+  });
+  const data: Todo = await response.json();
   return data;
 };
