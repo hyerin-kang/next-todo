58eefced9998ea27b4fe5b46646a0ef602470ea3
hyerin-kang <hkang0420@gmail.com>
2025-03-19 11:47:31 +0900

    feat : Tanstack Query의 prefetch로 SSR 구현하기

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,11 @@
+import { getTodoItem } from "@/api/todo-api";
 import TodoDetail from "@/components/todo/TodoDetail";
 import { Button } from "@/components/ui/button";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import Link from "next/link";
 
 interface DetailPageProps {
@@ -7,15 +13,23 @@ interface DetailPageProps {
 }
 const DetailPage = async ({ params }: DetailPageProps) => {
   const { id } = await params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos", id],
+    queryFn: () => getTodoItem(id),
+  });
   return (
-    <section>
-      <div className="container mx-auto space-y-2">
-        <TodoDetail id={id} />
-        <Link href={"/"}>
-          <Button className="w-full">돌아가기</Button>
-        </Link>
-      </div>
-    </section>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <section>
+        <div className="container mx-auto space-y-2">
+          <TodoDetail id={id} />
+          <Link href={"/"}>
+            <Button className="w-full">돌아가기</Button>
+          </Link>
+        </div>
+      </section>
+    </HydrationBoundary>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
+import { getTodos } from "@/api/todo-api";
 import TodoForm from "@/components/todo/TodoForm";
 import TodoList from "@/components/todo/TodoList";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 const HomePage = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
   return (
-    <div>
-      <div className="container mx-auto space-y-4 ">
-        <TodoForm />
-        <TodoList />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div>
+        <div className="container mx-auto space-y-4 ">
+          <TodoForm />
+          <TodoList />
+        </div>
       </div>
-    </div>
+    </HydrationBoundary>
   );
 };
 
