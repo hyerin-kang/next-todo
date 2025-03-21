b878862450b7f333e04eaee746cad555d17060af
hyerin-kang <hkang0420@gmail.com>
2025-03-21 20:44:03 +0900

    feat : 상태관리 로직 추가

--- components/todo/TodoFilterSwitch.tsx
+++ components/todo/TodoFilterSwitch.tsx
@@ -2,13 +2,23 @@
 import { Label } from "@radix-ui/react-label";
 import React, { useId } from "react";
 import { Switch } from "../ui/switch";
+import { useTodoFilterStore } from "@/store/useTodoFilterStore";
 
 const TodoFilterSwitch = () => {
   const switchId = useId();
+  const { filter, setFilter } = useTodoFilterStore();
+  const onCheckedChange = (checked: boolean) => {
+    setFilter(checked ? "completed" : "all");
+  };
   return (
     <div className="flex items-center space-x-2">
-      <Switch id={switchId} />
+      <Switch
+        id={switchId}
+        checked={filter === "completed"}
+        onCheckedChange={onCheckedChange}
+      />
       <Label htmlFor={switchId}>Completed</Label>
+      {filter}
     </div>
   );
 };
